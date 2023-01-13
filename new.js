d24e35949c4134410d027fff51950607ad748fc3
Barkha <barkhadeshmukh97@gmail.com>
2023-01-13 23:58:07 +0530

    explore

---
+++ new.js
@@ -0,0 +1,11 @@
+// console.log("hello")
+// alert("hiii")
+// var username = prompt("what is your name")
+// var welcome = "welcome mr/miss" + username
+// alert(welcome)
+
+var btntranslator = document.querySelector("btn-translate")
+btntranslator.addEventListener("click" ,function Eventhandler(){
+    alert("clicked")
+})
+

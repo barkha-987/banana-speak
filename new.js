093ae48fda2f1c88b703cc93b87adf497648a9c3
Barkha <barkhadeshmukh97@gmail.com>
2023-01-14 14:32:33 +0530

    added

--- new.js
+++ new.js
@@ -1,11 +1,6 @@
-// console.log("hello")
-// alert("hiii")
-// var username = prompt("what is your name")
-// var welcome = "welcome mr/miss" + username
-// alert(welcome)
 
-var btntranslator = document.querySelector("btn-translate")
-btntranslator.addEventListener("click" ,function Eventhandler(){
-    alert("clicked")
-})
 
+var btntranslator = document.querySelector("#btnn");
+
+
+ console.log(btntranslator)

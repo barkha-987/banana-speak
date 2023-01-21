b082c99a50d57178d3aa7ad0fbe1f2827176bcd3
Barkha <barkhadeshmukh97@gmail.com>
2023-01-21 09:25:27 +0530

    add javascript

--- new.js
+++ new.js
@@ -1,6 +1,13 @@
 
 
-var btntranslator = document.querySelector("#btnn");
+ var btnTranslate = document.querySelector("#btnprimary");
+ console.log(btnTranslate);
+// btntranslator.addEventListener("click",function clickEventHandler(){
+//     console.log("clicked");
+// })
+// var stu = prompt("what is your name:-");
+// obclick.alert("welcome"+stu+"#oktxt".value);
 
 
- console.log(btntranslator)
+
+

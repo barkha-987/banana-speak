36af52b18d22ed0700cd8fc970f08dd96061cb8f
Barkha <barkhadeshmukh97@gmail.com>
2023-01-21 20:23:05 +0530

    remove changes

--- new.js
+++ new.js
@@ -1,12 +1,4 @@
  var btnTranslate = document.querySelector("#btn-primary");
- console.log(btnTranslate);
-
-// btntranslator.addEventListener("click",function clickEventHandler(){
-//     console.log("clicked");
-// })
-// var stu = prompt("what is your name:-");
-// onclick.alert("welcome"+stu+"#oktxt".value);
-
-
+ Console.log(btnTranslate);
 
 

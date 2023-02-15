aece1753547e5facadf1000e2869bc718b782ab5
Barkha <barkhadeshmukh97@gmail.com>
2023-02-15 22:42:52 +0530

    create project

--- new.js
+++ new.js
@@ -1,10 +1,28 @@
+var button = document.querySelector("#btn-trans");
+var textarea= document.querySelector("#txtarea");
+var translate = document.querySelector("#translate");
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+function translateValue(text)
+{
 
- var btnTranslate = document.querySelector("#btn-primary");
-    // console.log(btnTranslate);
-   var input = btnTranslate.addEventListner("click",function addedEvents(){
-    console.log("clicked");
-   })
-console.log(input);
+return serverURL+ "?" + "text="+text
+
+}
+ function errorHandler(error){
+   console.log("error occure" + error);
+ }
+ function Eventlistner(){
+        var inputText = textarea.value; 
+        fetch(translateValue(inputText))
+        .then(response=>response.json())
+         .then(json=>
+         {var  translatedd = json.contents.translated;
+         translate.innerText = translatedd})
+        .catch(errorHandler)
+
+ }
+button.addEventListener("click", Eventlistner);
 
 
 
